45ced0cf955332f91ab58ec156263cea917fa136
Cole <Cole@DESKTOP-J2DB18V.domain>
2017-03-31 15:47:28 -0500

    initial commit

---
+++ src/basketballsimulator/Team.java
@@ -0,0 +1,23 @@
+package basketballsimulator;
+
+/**
+ * Team:    represents teams and their attributes
+ * @author  Cole Petersen
+ */
+
+public class Team {
+    
+    String name;
+    int ORate;
+    int DRate;
+    double ExpScore;
+    int score;
+    
+    /**
+     * Team:    team constructor
+     */
+    public Team(){
+        
+    }
+    
+}
